6dc31a202333592c5066f926da60798eef78520e
Vitalii <vitalii.ilchenko@gmail.com>
2018-09-20 10:21:49 +0300

    first/reasons/problems/expert/reviews/popup

--- app/js/common.js
+++ app/js/common.js
@@ -2,47 +2,47 @@ $(document).ready(function() {
     console.log( "ready!" );
 });
 
-$(function () { 
-	$('.plus').on('click', function() { 
+$(function () {
+	$('.plus').on('click', function() {
 		console.log('1');
-		$(this).siblings('.reviews-body').find(".reviews-text").toggleClass('active'); 
+		$(this).siblings('.reviews-body').find(".reviews-text").toggleClass('active');
 		$(this).toggleClass('active')
 	});
 	$(".table_slider").slick( {
 		slidesToShow: 4,
 		slidesToScroll: 1,
-		dots: false, 
-		touchMove: false, 
+		dots: false,
+		touchMove: false,
 		infinite: false,
-		responsive: [ 
+		responsive: [
 			{
-				breakpoint: 1023, 
+				breakpoint: 1023,
 				settings: {
-					slidesToShow: 3, 
+					slidesToShow: 3,
 					touchMove: false
 				}
-			}, 
+			},
 			{
-				breakpoint: 767, 
+				breakpoint: 767,
 				settings: {
-					slidesToShow: 2, 
+					slidesToShow: 2,
 					touchMove: false
 				}
-			}, 
+			},
 			{
-				breakpoint: 478, 
+				breakpoint: 478,
 				settings: {
-					slidesToShow: 1, 
+					slidesToShow: 1,
 					touchMove: false
 				}
 			}
 		]
-	}); 
+	});
 	$('.reviews-inner').slick( {
 		slidesToShow: 3,
 		slidesToScroll: 1,
-		dots: false, 
-		touchMove: false, 
+		dots: false,
+		touchMove: false,
 		infinite: true
 	}) ;
 
